cb06478839ef2d527f1122b3deba4ee92af4c718
DESKTOP-Darek <darek710@wp.pl>
2017-11-30 09:25:19 +0100

    e

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -6,11 +6,21 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour {
 
+    PlayerSetup m_PlayerSetup;
+    PlayerHealth m_PlayerHealth;
+    PlayerShoot m_PlayerShoot;
+
     Vector3 m_moveDirection;
     Vector3 m_turretDirection;
-    Plane m_plane;
+    Vector3 m_originalSpawnPos;
+
+    NetworkStartPosition[] m_spawnPosArray;
 
     LayerMask m_groundMask;
+
+    public GameObject m_SpawnEffectPrefab;
+
+    public int m_Score;
 
     [SerializeField]
     Transform m_turretTransform;
@@ -30,27 +40,37 @@ public class PlayerController : NetworkBehaviour {
 
     private void Awake()
     {
+        m_PlayerHealth = GetComponent<PlayerHealth>();
+        m_PlayerSetup = GetComponent<PlayerSetup>();
+        m_PlayerShoot = GetComponent<PlayerShoot>();
         m_RB = GetComponent<Rigidbody>();
-        m_plane = new Plane(Vector2.up, new Vector3(0f, 2.3f, 0f));
         m_groundMask = LayerMask.GetMask("Ground");
+
+    }
+
+    public override void OnStartLocalPlayer()
+    {
+        m_spawnPosArray = FindObjectsOfType<NetworkStartPosition>();
+        m_originalSpawnPos = transform.position;
     }
 
     private void Update()
     {
-        if (!isLocalPlayer)
+        if (!isLocalPlayer || m_PlayerHealth.m_IsDead)
             return;
 
         GetTurretDirection();
         GetMoveDirection();
         FaceDirection(m_turretTransform, m_turretDirection, m_turretRotationSpeed);
         FaceDirection(m_bodyTransform, m_moveDirection, m_bodyRotationSpeed);
+        m_PlayerShoot.Tick();
     }
 
 
 
     private void FixedUpdate()
     {
-        if (!isLocalPlayer)
+        if (!isLocalPlayer || m_PlayerHealth.m_IsDead)
             return;
 
         GetMoveDirection();
@@ -93,5 +113,41 @@ public class PlayerController : NetworkBehaviour {
             return;
         Quaternion direction = Quaternion.LookRotation(_dir);
         _transform.rotation = Quaternion.Slerp(_transform.rotation, direction, _speed * Time.deltaTime);
+    }
+
+    public void Disable()
+    {
+        StartCoroutine(RespawnCO());       
+    }
+
+    IEnumerator RespawnCO()
+    {
+
+        transform.position = GetSpawnPosition() ;
+        m_RB.velocity = Vector3.zero;
+        yield return new WaitForSeconds(2f);
+        if (m_SpawnEffectPrefab != null)
+        {
+            GameObject effectinstance = Instantiate(m_SpawnEffectPrefab, transform.position = Vector3.up * .5f, Quaternion.identity);
+            Destroy(effectinstance, 3f);
+        }
+        yield return new WaitForSeconds(.5f);
+        m_PlayerHealth.ResetOnSpawn();
+        
+    }
+
+    Vector3 GetSpawnPosition()
+    {
+        if (m_spawnPosArray != null && m_spawnPosArray.Length > 0)
+        {
+            NetworkStartPosition pos = m_spawnPosArray[UnityEngine.Random.Range(0, m_spawnPosArray.Length)];
+            return pos.transform.position;
+        }
+        return m_originalSpawnPos;
+    }
+
+    public string GetName()
+    {
+        return m_PlayerSetup.m_PlayernameText.text;
     }
 }
